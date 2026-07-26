8186bff38e6f23342f352e1c982eba3c4ecaf84d
Win <win@gmail.com>
2026-07-26 17:08:32 +0500

    done boss

--- f.py
+++ f.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 def greet(name):
 	print(f"yo {name}. Git test activated")
-	print("updated from second machine")
-	print("sirf print hi krwate rhenge flhl shayd")
 if __name__=="__main__":
 	greet("win")
