5d41ec6eefa886ebd69224ca4a0bb9cbd371fa5d
stitch <stitch@gmail.com>
2026-07-26 17:04:50 +0500

    tere bad wali line change ki he last ki dono line hata de awein hen

--- f.py
+++ f.py
@@ -2,5 +2,6 @@
 def greet(name):
 	print(f"yo {name}. Git test activated")
 	print("updated from second machine")
+	print("sirf print hi krwate rhenge flhl shayd")
 if __name__=="__main__":
 	greet("win")
