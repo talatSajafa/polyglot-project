f898f4ee02a75fe904de7dd1dae3077cc1d66bd5
stitch <stitch@gmail.com>
2026-08-05 18:53:16 +0500

    clean: keep only benchmark.txt and README.md

--- f.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-def greet(name):
-	print(f"yo {name}. Git test activated")
-	print("wapis dal dia")
-	print("b1 check kr baki ok he")
-if __name__=="__main__":
-	greet("win")
